95570f093766592eedb7317e721060fb862e7acf
iratatuii <andreikarpenkin123@gmail.com>
2026-03-02 10:41:00 +0300

    chore: add webserver to ALLOWED_HOSTS

--- hexlet_code/settings.py
+++ hexlet_code/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-k7a4l+kdh$uf^^ry(i_@bah!k6kq=4q$+7-#txg5vo7%_+=s8t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "webserver"]
 
 # Application definition
 
