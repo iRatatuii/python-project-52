dc2c5ce452ce9ba7f77bd1aa7b5a9b766b70b57c
iratatuii <andreikarpenkin123@gmail.com>
2026-03-02 13:13:08 +0300

    feat: enhance admin interface for all models

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
 from .models import Status, Label, Task
 
-admin.site.register(Status)
-admin.site.register(Label)
-admin.site.register(Task)
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "created_at")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+    ordering = ("id",)
+
+
+@admin.register(Label)
+class LabelAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "created_at")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+    ordering = ("id",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "status", "author", "executor", "created_at")
+    list_display_links = ("id", "name")
+    list_filter = ("status", "author", "executor", "labels")
+    search_fields = ("name", "description")
+    filter_horizontal = ("labels",)  # Удобный виджет для выбора меток
+    readonly_fields = ("created_at",)
+    fieldsets = (
+        ("Основная информация", {"fields": ("name", "description", "created_at")}),
+        ("Связи", {"fields": ("status", "author", "executor", "labels")}),
+    )
+    ordering = ("-created_at",)
